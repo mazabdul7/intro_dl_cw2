473bebe6b747f4b63ce0dddb21c37812654fe393
myrtokonst1 <ucabm@ucl.ac.uk>
2022-01-08 00:10:42 +0000

    cross validation of UNet for 10 epochs

--- train_baseline.py
+++ train_baseline.py
@@ -11,6 +11,7 @@ from utils.config import config
 from utils.loader import DataLoader
 from utils.tools import generator_img_baseline_data, dice_binary
 
+unet_model_path = 'model_weights/UNet/CV'
 batch_size = config['unet_batch_size']
 batch_size_val = config['unet_batch_size']
 
@@ -19,15 +20,15 @@ num_val = config['num_val']
 
 cross_validation_folds = config['cross_validation_folds']
 
-num_epochs = 4
+num_epochs = 10
 
-loss_per_fold = []
-dice_binary_per_fold = []
-accuracy_per_fold = []
+# Instantiate and build model
+input_shape = config['input_shape']
+model = UNet(input_shape=input_shape).build_model()
 
-val_loss_per_fold = []
-val_dice_binary_per_fold = []
-val_accuracy_per_fold = []
+
+def get_mean_metric(metric, model_histories):
+    return np.mean(model_histories[i][metric] for i in range(cross_validation_folds)) * 100
 
 model_histories = []
 for fold in range(cross_validation_folds):
@@ -44,55 +45,44 @@ for fold in range(cross_validation_folds):
     val_imgs = loader2.get_image_ds().repeat()
     val_masks = loader2.get_mask_ds().repeat()
 
-    # Instantiate and build model
-    input_shape = config['input_shape']
-    model = UNet(input_shape=input_shape).build_model()
-
-    model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=[{'Accuracy': 'accuracy', 'dice': dice_binary}])
+    model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=[{'accuracy': 'accuracy', 'dice_binary': dice_binary}])
     model.summary()
 
     # fit training and validation data
     # we have a checkpointer that saves the best unet model
-    checkpointer = ModelCheckpoint(f'model_weights/best_UNet_model_{fold}.h5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(f'{unet_model_path}/best_UNet_model_{fold+1}.h5', verbose=1, save_best_only=True)
     history = model.fit(generator_img_baseline_data(train_imgs, train_masks), validation_data=generator_img_baseline_data(val_imgs, val_masks), batch_size=batch_size, epochs=num_epochs, callbacks=[checkpointer], steps_per_epoch=num_train//batch_size, validation_steps=num_val//batch_size_val)
-    loss_per_fold.append(history.history['loss'])
-    accuracy_per_fold.append(history.history['accuracy'])
-    dice_binary_per_fold.append(history.history['dice_binary'])
 
-    val_loss_per_fold.append(history.history['val_loss'])
-    val_accuracy_per_fold.append(history.history['val_accuracy'])
-    val_dice_binary_per_fold.append(history.history['val_dice_binary'])
-
-    model_histories.append(history)
+    model_histories.append(history.history)
 
 print('Scores per fold:\n')
 for i in range(cross_validation_folds):
-    print(f'For fold {i}:\n')
+    history = model_histories[i]
+    print(f'For fold {i+1}:')
+
     print(f'\tTraining:')
-    print(f'\t\tAccuracy: {accuracy_per_fold[i]*100} %')
-    print(f'\t\tLoss: {loss_per_fold[i]*100} %')
-    print(f'\t\tDice Binary: {dice_binary_per_fold[i]*100} %\n')
+    print(f'\t\tAccuracy: {np.mean(history["accuracy"])*100} %')
+    print(f'\t\tLoss: {np.mean(history["loss"])*100} %')
+    print(f'\t\tDice Binary: {np.mean(history["dice_binary"])*100} %\n')
 
     print(f'\tValidation:')
-    print(f'\t\tAccuracy: {val_accuracy_per_fold[i]*100} %')
-    print(f'\t\tLoss: {val_loss_per_fold[i]*100} %')
-    print(f'\t\tDice Binary: {val_dice_binary_per_fold[i]*100} %')
+    print(f'\t\tAccuracy: {np.mean(history["val_accuracy"])*100} %')
+    print(f'\t\tLoss: {np.mean(history["val_loss"])*100} %')
+    print(f'\t\tDice Binary: {np.mean(history["val_dice_binary"])*100} %\n')
 
-print('Average Scores for UNET:\n')
+print('Average Scores for UNET:')
 print(f'\tTraining:')
-print(f'\t\tAccuracy: {np.mean(accuracy_per_fold) * 100} %')
-print(f'\t\tLoss: {np.mean(loss_per_fold) * 100} %')
-print(f'\t\tDice Binary: {np.mean(dice_binary_per_fold) * 100} %\n')
+print(f'\t\tAccuracy: {get_mean_metric("accuracy", model_histories)} %')
+print(f'\t\tLoss: {get_mean_metric("loss", model_histories)} %')
+print(f'\t\tDice Binary: {get_mean_metric("dice_binary", model_histories)} %\n')
 
 print(f'\tValidation:')
-print(f'\t\tAccuracy: {np.mean(val_accuracy_per_fold) * 100} %')
-print(f'\t\tLoss: {np.mean(val_loss_per_fold) * 100} %')
-print(f'\t\tDice Binary: {np.mean(val_dice_binary_per_fold) * 100} %')
+print(f'\t\tAccuracy: {get_mean_metric("val_accuracy", model_histories)} %')
+print(f'\t\tLoss: {get_mean_metric("val_loss", model_histories)} %')
+print(f'\t\tDice Binary: {get_mean_metric("val_dice_binary", model_histories)} %\n')
 
 # pick best model out of the 3 - used loss we can use some other factor
-best_model_idx = np.argmin(loss_per_fold)
-# load best model
-model = load_model(f'model_weights/best_UNet_model_{best_model_idx}.h5')
+best_model_idx = np.argmin(np.mean(model_histories[i]["loss"]) for i in range(cross_validation_folds))
 
 best_model_history = model_histories[best_model_idx]
 epochs = np.arange(0, num_epochs)
@@ -112,6 +102,9 @@ plt.plot(epochs, best_model_history.history['val_dice_binary'], label='val_dice_
 plt.legend()
 plt.show()
 
+# load best model
+best_model = load_model(f'{unet_model_path}/best_UNet_model_{best_model_idx}.h5', custom_objects={"dice_binary": dice_binary})
+
 # fixme not sure if this is right
 loader = DataLoader(batch_size=batch_size, batch_size_val=batch_size_val, CrossVal=0, CV_iteration=0)
 
@@ -119,7 +112,7 @@ loader = DataLoader(batch_size=batch_size, batch_size_val=batch_size_val, CrossV
 img_ds_test = loader.get_image_ds(test_mode=True)
 masks_ds_test = loader.get_mask_ds(test_mode=True)
 
-test_preds = model.predict(img_ds_test, batch_size=batch_size)
+test_preds = best_model.predict(img_ds_test, batch_size=batch_size)
 formatted_test_preds = (test_preds >= 0.5).astype(np.uint8)
 
 seg_acc = np.sum(formatted_test_preds == masks_ds_test)/(masks_ds_test.shape[0]*(255*255))
