a1e1d69b67b51b2d665792ddd620a49394c31c93
myrtokonst1 <ucabm@ucl.ac.uk>
2022-01-08 13:31:39 +0000

    cleaned up

--- train_baseline.py
+++ train_baseline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, CSVLogger
 from keras.models import load_model
 
 import matplotlib.pyplot as plt
@@ -10,8 +10,9 @@ from models.unet_model import UNet
 from utils.config import config
 from utils.loader import DataLoader
 from utils.tools import generator_img_baseline_data, dice_binary
+from utils.unet_helper_functions import get_unet_model_path, get_unet_training_log_path, print_model_metric_analysis, \
+    print_models_average_metric_analysis
 
-unet_model_path = 'model_weights/UNet/CV'
 batch_size = config['unet_batch_size']
 batch_size_val = config['unet_batch_size']
 
@@ -20,7 +21,8 @@ num_val = config['num_val']
 
 cross_validation_folds = config['cross_validation_folds']
 
-num_epochs = 5
+num_epochs = 10
+
 
 # Instantiate and build model
 input_shape = config['input_shape']
@@ -30,6 +32,8 @@ model = UNet(input_shape=input_shape).build_model()
 def get_mean_metric(metric, model_histories):
     return np.mean(model_histories[i][metric] for i in range(cross_validation_folds)) * 100
 
+# model histories is a list
+# including the history of the best UNet model per fold
 model_histories = []
 for fold in range(cross_validation_folds):
     print(f"-------------------- start cross val {fold + 1} --------------------")
@@ -45,52 +49,41 @@ for fold in range(cross_validation_folds):
     val_imgs = loader2.get_image_ds().repeat()
     val_masks = loader2.get_mask_ds().repeat()
 
-    ### CLEARS OLD MODELS IN CACHE
+    # CLEARS OLD MODELS IN CACHE
     tf.keras.backend.clear_session()
 
     model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=[{'accuracy': 'accuracy', 'dice_binary': dice_binary}])
     model.summary()
 
-    # fit training and validation data
     # we have a checkpointer that saves the best unet model
-    checkpointer = ModelCheckpoint(f'{unet_model_path}/best_UNet_model_{fold+1}.h5', verbose=1, save_best_only=True)
-    history = model.fit(generator_img_baseline_data(train_imgs, train_masks), validation_data=generator_img_baseline_data(val_imgs, val_masks), batch_size=batch_size, epochs=num_epochs, callbacks=[checkpointer], steps_per_epoch=num_train//batch_size, validation_steps=num_val//batch_size_val)
+    # and a cvs_logger that saves the history of each unet model
+    checkpointer = ModelCheckpoint(get_unet_model_path(fold), verbose=1, save_best_only=True)
+    csv_logger = CSVLogger(get_unet_training_log_path(fold), separator=',', append=False)
+
+    # fit training and validation data
+    history = model.fit(generator_img_baseline_data(train_imgs, train_masks), validation_data=generator_img_baseline_data(val_imgs, val_masks), batch_size=batch_size, epochs=num_epochs, callbacks=[checkpointer, csv_logger], steps_per_epoch=num_train//batch_size, validation_steps=num_val//batch_size_val)
 
     model_histories.append(history.history)
 
 
+# Printing the Metric analysis per fold and average
 print('Scores per fold:\n')
 for i in range(cross_validation_folds):
     history = model_histories[i]
     print(f'For fold {i+1}:')
-
-    print(f'\tTraining:')
-    print(f'\t\tAccuracy: {np.mean(history["accuracy"])*100} %')
-    print(f'\t\tLoss: {np.mean(history["loss"])*100} %')
-    print(f'\t\tDice Binary: {np.mean(history["dice_binary"])*100} %\n')
-
-    print(f'\tValidation:')
-    print(f'\t\tAccuracy: {np.mean(history["val_accuracy"])*100} %')
-    print(f'\t\tLoss: {np.mean(history["val_loss"])*100} %')
-    print(f'\t\tDice Binary: {np.mean(history["val_dice_binary"])*100} %\n')
+    print_model_metric_analysis(history)
 
 print('Average Scores for UNET:')
-print(f'\tTraining:')
-print(f'\t\tAccuracy: {np.mean([model_histories[i]["accuracy"] for i in range(cross_validation_folds)]) * 100} %')
-print(f'\t\tLoss: {np.mean([model_histories[i]["loss"] for i in range(cross_validation_folds)]) * 100} %')
-print(f'\t\tDice Binary: {np.mean([model_histories[i]["dice_binary"] for i in range(cross_validation_folds)]) * 100} %\n')
-
-print(f'\tValidation:')
-print(f'\t\tAccuracy: {np.mean([model_histories[i]["val_accuracy"] for i in range(cross_validation_folds)]) * 100} %')
-print(f'\t\tLoss: {np.mean([model_histories[i]["val_loss"] for i in range(cross_validation_folds)]) * 100} %')
-print(f'\t\tDice Binary: {np.mean([model_histories[i]["val_dice_binary"] for i in range(cross_validation_folds)]) * 100} %\n')
+print_models_average_metric_analysis(model_histories, cross_validation_folds)
 
 # pick best model out of the 3 - used loss we can use some other factor
-best_model_idx = np.argmin(np.mean(model_histories[i]["loss"]) for i in range(cross_validation_folds))
+average_loss_per_model = [np.mean(model_histories[i]["loss"]) for i in range(cross_validation_folds)]
+best_model_idx = np.argmin(average_loss_per_model)
 
 best_model_history = model_histories[best_model_idx]
 epochs = np.arange(0, num_epochs)
-# plot all metrics
+
+# plot all metrics for the best model
 plt.plot(epochs, best_model_history['loss'], label='loss')
 plt.plot(epochs, best_model_history['val_loss'], label='val_loss')
 plt.legend()
@@ -107,22 +100,23 @@ plt.legend()
 plt.show()
 
 # load best model
-best_model = load_model(f'{unet_model_path}/best_UNet_model_{best_model_idx+1}.h5', custom_objects={"dice_binary": dice_binary})
+best_model = load_model(get_unet_model_path(best_model_idx+1), custom_objects={"dice_binary": dice_binary})
 
-# fixme not sure if this is right
+# Load and get test data
 loader = DataLoader(batch_size=batch_size, batch_size_val=batch_size_val, CrossVal=0, CV_iteration=0)
 
-# get test data
 img_ds_test = loader.get_image_ds(test_mode=True)
 masks_ds_test = loader.get_mask_ds(test_mode=True)
 
+# Predict using best UNet model
 test_preds = best_model.predict(img_ds_test, batch_size=batch_size)
 formatted_test_preds = (test_preds >= 0.5).astype(np.uint8)
 
-seg_acc = np.sum(formatted_test_preds == masks_ds_test)/(masks_ds_test.shape[0]*(255*255))
-print(f"Accuracy {seg_acc*100}%")
+# Compute and print test accuracy
+test_accuracy = np.sum(formatted_test_preds == masks_ds_test) / (masks_ds_test.shape[0] * (255 * 255))
+print(f"Accuracy {test_accuracy * 100}%")
 
-# print 3 ground truth/prediction masks
+# plot 3 ground truth/prediction masks
 idx = list(range(img_ds_test.shape[0]))
 random.shuffle(idx)
 for i in range(3):
